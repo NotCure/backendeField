dac821e0e97332bffe9db7cfa11dcf4574eb319d
Ebi Albosify <167235751+NotCure@users.noreply.github.com>
2025-06-28 18:37:22 +0200

    Update steamRouter.ts

--- src/api/steam/steamRouter.ts
+++ src/api/steam/steamRouter.ts
@@ -1,30 +1,25 @@
 import express, { Router } from "express";
-import steamAuthController from "@/api/steam/steamController"; // Adjust path accordingly
+import steamAuthController from "@/api/steam/steamController"; 
 import steam from "steam-login";
 import { env } from "@/common/utils/envConfig";
 
 const steamRouter: Router = express.Router();
 
-// Middleware for Steam
 steamRouter.use(
   steam.middleware({
-    realm: "https://backendefield.onrender.com/", // Your backend URL
-    verify: `https://backendefield.onrender.com/steam/auth/steam/return`, // The callback route after Steam login
-    apiKey: process.env.STEAM_API_KEY!, // Your Steam API key
+    realm: "https://backendefield.onrender.com/", 
+    verify: `https://backendefield.onrender.com/steam/auth/steam/return`, 
+    apiKey: process.env.STEAM_API_KEY!
   })
 );
 
-// Steam login route
 steamRouter.get("/auth/steam", steam.authenticate(), steamAuthController.login);
 
-// Steam callback route after successful Steam login
 steamRouter.get("/auth/steam/return", steam.verify(), steamAuthController.steamCallback);
 
-// Route for initial verification with Discord ID
 steamRouter.get("/verify/:token", steamAuthController.verify);
 
 
-// Steam logout route
 steamRouter.get("/auth/logout", steam.enforceLogin("/"), steamAuthController.logout);
 
 export default steamRouter;
